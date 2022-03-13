98269085be8d88a65f9eb99306673e28b13de8b1
Manao-dev <pm@manao.by>
2022-03-13 19:27:41 +0300

    add atm

--- components/Planet/Planet.tsx
+++ components/Planet/Planet.tsx
@@ -1,56 +1,126 @@
 import { Fragment } from "react";
 import classes from "./Planet.module.sass";
 import { Suspense, useRef } from "react";
-import { Canvas, useFrame, useLoader } from "react-three-fiber";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import * as three from "three";
-import { TextureLoader } from "three";
+import { AdditiveBlending, BackSide, BufferGeometry, SphereBufferGeometry, TextureLoader } from "three";
+import glsl from 'glslify'
+
+const shaders = {
+    vertexShader: 
+        glsl`
+            varying vec2 vertexUV;
+            varying vec3 vertexNormal;
+            void main() {
+                vertexUV =  uv;
+                vertexNormal = normalize(normalMatrix * normal);
+                gl_Position = projectionMatrix * modelViewMatrix * vec4(
+                    position, 1.0
+                );
+            }
+        `,
+    fragmentShader: 
+        glsl`
+            uniform sampler2D globalTexture;
+            varying vec2 vertexUV;
+            varying vec3 vertexNormal;
+            void main() {
+
+                float intensity = 1.05 - dot(vertexNormal, vec3(0.0, 0.0, 1.0));
+                vec3 atmosphere = vec3(0.3, 0.6, 1.0) * pow(intensity, 1.5);
+
+                gl_FragColor = vec4(atmosphere + texture2D(globalTexture, vertexUV).xyz, 1.0);
+            }
+        `,
+    atmosphereVertex: glsl`
+        varying vec3 vertexNormal;
+        void main() {
+            vertexNormal = normalize(normal);
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(
+                position, 1
+            );
+    }
+    `,
+    atmosphereFragment: glsl`
+        varying vec3 vertexNormal;
+        void main() {
+            float intensity = pow(0.6 - dot(vertexNormal, vec3(0, 0, 1.0)), 2.0);
+            gl_FragColor = vec4(0.3, 0.6, 1.0, 1) * intensity;
+        }
+    `
+}
 
 const Earth = () => {
-  const earth = useRef<three.Mesh>();
+    const earth = useRef<three.Mesh>();
 
-  useFrame(() => {
-    earth.current!.rotation.y += 0.001;
-  });
+    useFrame(() => {
+        earth.current!.rotation.y += 0.001;
+    });
 
-  const [colorMap] = useLoader(TextureLoader, ["/earth.jpeg"]);
+    return (
+        <mesh ref={earth} position={[-1, -1, 0]}>
+            <sphereBufferGeometry args={[1, 50, 50]} />
+            <shaderMaterial 
+                vertexShader={shaders.vertexShader}
+                fragmentShader={shaders.fragmentShader}
+                uniforms={{globalTexture: {
+                    value: new TextureLoader().load("/earth.jpeg")
+                }}}
+            />
+        </mesh>
+    );
+};
 
-  return (
-    <mesh ref={earth} position={[-1, -1, 0]}>
-      <sphereBufferGeometry args={[1, 50, 50]} />
-      <meshStandardMaterial map={colorMap} />
-    </mesh>
-  );
+const Atmosphere = () => {
+
+    const atm = useRef<three.Mesh>();
+
+    useFrame(() => {
+        atm.current?.scale.set(1.1, 1.1, 1.1)
+    });
+
+    return (
+        <mesh ref={atm} position={[-1, -1, 0]}>
+            <sphereBufferGeometry args={[1, 50, 50]} />
+            <shaderMaterial 
+                vertexShader={shaders.atmosphereVertex}
+                fragmentShader={shaders.atmosphereFragment}
+                blending={AdditiveBlending}
+                side={BackSide}
+            />
+        </mesh>
+    );
 };
 
 const Scene = () => {
-  return (
-    <Fragment>
-      <pointLight intensity={0.5} position={[3, 3, 5]} />
-      <Earth />
-      {/* <Atmoshpere /> */}
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            <pointLight intensity={0.5} position={[1, 1, 5]} />
+            <Atmosphere />
+            <Earth />
+        </Fragment>
+    );
 };
 
 const Planet = () => {
-  return (
-      <div className={classes.root}>
-        <Canvas
-          camera={{
-            near: 0.5,
-            far: 500,
-            zoom: 2,
-          }}
-          onCreated={({ gl }) => {
-            gl.setClearColor("#fff)", 0);
-          }}
-        >
-          <Suspense fallback={null}>
-            <Scene />
-          </Suspense>
-        </Canvas>
-      </div>
-  );
+    return (
+        <div className={classes.root}>
+            <Canvas
+                camera={{
+                    near: 0.2,
+                    far: 500,
+                    zoom: 0.8,
+                }}
+                onCreated={({ gl }) => {
+                    gl.setClearColor("rgb(255, 0, 0)", 0);
+                }}
+            >
+                <Suspense fallback={null}>
+                    <Scene />
+                </Suspense>
+            </Canvas>
+        </div>
+    );
 };
 
 export default Planet;
--- components/Planet/shaders/atmosphere.ts
+++ components/Planet/shaders/atmosphere.ts
@@ -1,18 +1,25 @@
 import { Shaders, Node, GLSL } from "gl-react";
 
 export const shaders = Shaders.create({
+    vertex: {
+      frag: GLSL`
+        void main() {
+          gl_Position = vec4(1, 0, 0, 1);
+        }
+      `
+    },
     atmFragment: {
-        frag: GLSL `
+        frag: GLSL`
         varying vec3 vertexNormal;
         void main() {
             float intensity = pow(0.5 - dot(vertexNormal, vec3(0, 0, 1.0)), 2.0);
-            gl_FragColor = vec4(0.3, 0.6, 1.0, 1.0) * intensity
+            gl_FragColor = vec4(0.3, 0.6, 1.0, 1.0) * intensity;
           )
         }
       `
     },
     atm: {
-        frag: GLSL `
+        frag: GLSL`
             varying vec2 vertexUV;
             varying vec3 vertexNormal;
             void main() {
